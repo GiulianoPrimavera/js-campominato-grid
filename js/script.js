00499aab6abb444a7e75a8f6408f9a92c06a4e1a
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-19 15:33:45 +0200

    adds addEventListener for the submit button, read the value of the difficulty option, based on that value creates a let variable with the number of the boxes that needs to be created

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const startGame = document.getElementById("start_game");
+const difficultyOption = document.getElementById("difficulty_option");
+
+//al click del bottone "invia" devo leggere il valore del livello di difficoltà del select
+startGame.addEventListener("click", function(){
+    //leggo il valore che si trova nell'opzione scelta dall'utente e lo assegno a una variabile
+    const difficultyLevel = difficultyOption.value;
+
+    console.log("l'utente ha scelto il livello di difficoltà", difficultyLevel);
+
+    //creo una variabile che tenga conto del numero di quadrati da creare in base al livello di difficoltà
+    let nBoxes;
+
+    //assegno alla variabile nBoxes il numero di scatole che voglio creare
+    if(difficultyLevel === "1"){
+        //assegno alla variabile "nBoxes" il valore 49
+        nBoxes = 49;
+    }else if(difficultyLevel === "2"){
+        //assegno alla variabile "nBoxes" il valore 81
+        nBoxes = 81;     
+    }else if(difficultyLevel === "3"){
+        //assegno alla variabile "nBoxes" il valore 100
+        nBoxes = 100;
+    }
+})
+
+
+
+/*
+
+if (difficultyLevel === "1"){
+    //crea 49 celle
+}else if (difficultyLevel === "2"){
+    //crea 81 celle
+}else if (difficultyLevel === "3"){
+    //crea 100 celle
+}
+
+ */
